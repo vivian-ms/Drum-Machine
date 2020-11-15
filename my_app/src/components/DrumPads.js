8db04c39f993f9666566fe669fdd797e0342134e
vivian-ms <56147408+vivian-ms@users.noreply.github.com>
2020-11-15 00:03:16 -0800

    Added onClick to buttons to store the name of the sound played

--- my_app/src/components/DrumPads.js
+++ my_app/src/components/DrumPads.js
@@ -1,7 +1,11 @@
-const DrumPads = ( {sound_bank} ) => {
+const DrumPads = ( {sound_bank, soundPlayed} ) => {
+  const clickHandler = evt => {
+    soundPlayed(evt.target.dataset.name);
+  }
+
   const pads = sound_bank.map((sound, i) => {
     return (
-      <button key={i} id={sound.id} className="drum-pad" data-key={sound.letter}>
+      <button key={i} id={sound.id} className="drum-pad" data-key={sound.letter} data-name={sound.name} onClick={clickHandler}>
         <audio id={sound.letter} className="clip" src={sound.url}></audio>
         {sound.letter}
       </button>
