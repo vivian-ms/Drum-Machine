6b7bb79d3f3161ef24493108b2d720c5108d4610
vivian-ms <56147408+vivian-ms@users.noreply.github.com>
2020-11-14 23:24:39 -0800

    Created basic structure of the sound array

--- my_app/src/App.js
+++ my_app/src/App.js
@@ -1,3 +1,60 @@
+const sound_bank = [
+  {
+    letter: 'Q',
+    id: 'sound_1',
+    name: 'Sound 1',
+    url: '',
+  },
+  {
+    letter: 'W',
+    id: 'sound_2',
+    name: 'Sound 2',
+    url: '',
+  },
+  {
+    letter: 'E',
+    id: 'sound_3',
+    name: 'Sound 3',
+    url: '',
+  },
+  {
+    letter: 'A',
+    id: 'sound_4',
+    name: 'Sound 4',
+    url: '',
+  },
+  {
+    letter: 'S',
+    id: 'sound_5',
+    name: 'Sound 5',
+    url: '',
+  },
+  {
+    letter: 'D',
+    id: 'sound_6',
+    name: 'Sound 6',
+    url: '',
+  },
+  {
+    letter: 'Z',
+    id: 'sound_7',
+    name: 'Sound 7',
+    url: '',
+  },
+  {
+    letter: 'X',
+    id: 'sound_8',
+    name: 'Sound 8',
+    url: '',
+  },
+  {
+    letter: 'C',
+    id: 'sound_9',
+    name: 'Sound 9',
+    url: '',
+  }
+];  // End sound_bank
+
 const App = () => {
   return (
     <div>
